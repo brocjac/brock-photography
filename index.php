13acac769152e25d3b352fec3c3923108a127f9f
brocjac <jbrockwell@my.wctc.edu>
2022-02-06 14:17:09 -0600

    redesign index page css and html

--- index.php
+++ index.php
@@ -11,12 +11,21 @@ require_once "includes/header-home.php";
     </section>
     <div id="skipTo" class="content">
         <div class="testimonial">
-            <img src="jacobb6.jpg" alt="portrait of Jacob Brockwell.  " width="206" height="309" id="selfPortrait">
-            <div>
-                <h1>About Me</h1>
-                <h2>Jacob Brockwell</h2>
-                <p>My name is Jacob Brockwell, I have always loved everything about the beauty of nature.  I am always there to capture natures lovely moments.  I have been doing photography since 2015 now and still going strong.  I started out taking pictures of my dog with my first DSLR.  Continuing on my journey, I photographed a wedding in 2015 - which was a great experience! Using my prior photography knowledge, I was able to capture some great moments for the happy couple. I won a “superior” award in Fine Arts for district competitions in 2016 and 2017. I also was awarded “superior” in nationals for the 2016 Fine Arts convention.  I also earned a 3rd place award for photography in a Skills USA district competition.  </p>
-            </div>
+            <div class="selfPortrait"></div>
+            <h2>Jacob Brockwell <br> The Photographer</h2>
+            <h3>My name is Jacob Brockwell, I have always loved everything about the beauty of nature. </h3>
+            <p class="descriptionTest">
+                I am always there to capture natures lovely moments.
+                I have been doing photography since 2015 now and still going strong.
+                I started out taking pictures of my dog with my first DSLR.
+            </p>
+            <p class="descriptionTest">
+                Continuing on my journey, went on with photographing a wedding in 2015 - which was a great experience! Using my prior photography knowledge, I was able to capture some great moments for the happy couple.
+            </p>
+            <p class="descriptionTest">
+                I won a “superior” award in Fine Arts for district competitions in 2016 and 2017. I also was awarded “superior” in nationals for the 2016 Fine Arts convention.
+                I also earned a 3rd place award for photography in a Skills USA district competition.
+            </p>
         </div>
         <?php
             include_once "includes/footer-home.php";
